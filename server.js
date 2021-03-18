c7f5b3125eff9df134d9e18785488465fab3cbca
Imferal <i.m.feral@gmail.com>
2021-03-19 01:44:04 +0300

    210319_5

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:3000",
+    "Access-Control-Allow-Origin": "*",
     methods: ["GET", "POST"]
   }
 })
